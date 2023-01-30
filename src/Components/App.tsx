12fb7cb4735bc960ffbe60d846686b61553605b7
Alycia Canavan <alycia.canavan@gmail.com>
2023-01-30 13:28:20 -0800

    add logo files, initial navbar

---
+++ src/Components/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Navbar';
+
+
+const App = () => {
+
+
+  return (
+    <div className="app">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,19 @@
+import './Navbar.css';
+import React from 'react';
+
+const Navbar = () => {
+
+
+    return (
+        <nav>
+            <img src=""/>
+            <a href="#about-me">about me</a>
+            <a href="my-work">my work</a>
+            <a href="#resume">resume</a>
+            <a href="#contact">contact me</a>
+        </nav>
+    )
+}
+
+
+export default Navbar;
