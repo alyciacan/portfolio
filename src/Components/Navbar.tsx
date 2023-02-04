e25def4acde83fc44d32cbe5400ec743120274c4
Alycia Canavan <alycia.canavan@gmail.com>
2023-02-03 21:55:47 -0800

    style ol

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -8,12 +8,20 @@ const Navbar = () => {
     return (
         <nav>
             <img src={ logo } alt="logo" className="logo"/>
-            <section className="links-container">
-                <a href="#about-me" className="nav-link">about me</a>
-                <a href="my-work" className="nav-link">my work</a>
-                <a href="#resume" className="nav-link">resume</a>
-                <a href="#contact" className="nav-link">contact me</a>
-            </section>
+            <ol className="links-container">
+                <li>
+                    <a href="#about-me" className="nav-link">about me</a>
+                </li>
+                <li>
+                    <a href="my-work" className="nav-link">my work</a>
+                </li>
+                <li>
+                    <a href="#resume" className="nav-link">resume</a>
+                </li>
+                <li>
+                    <a href="#contact" className="nav-link">contact me</a>
+                </li>
+            </ol>
         </nav>
     )
 }
